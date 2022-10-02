56d66516b2b916fcc87c65857fa69d0d42c4c867
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-10-01 23:37:05 -0300

    feat: utilizando profiles

--- src/main/java/com/meli/sales/MyConfiguration.java
+++ src/main/java/com/meli/sales/MyConfiguration.java
@@ -1,8 +1,10 @@
 package com.meli.sales;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MyConfiguration {
@@ -10,6 +12,12 @@ public class MyConfiguration {
   @Bean(name = "healthCheck")
   public String healthCheck(){
     return "pong";
+  }
+
+  @Bean
+  @Profile("development")
+  public CommandLineRunner execute(){
+    return args -> System.out.println("running configuration");
   }
 
 }
